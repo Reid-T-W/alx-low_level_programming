24e5712c850fd7d528f2329a58a38a7b070211df
Reid-T-W <rediettadesse100@gmail.com>
2022-03-11 13:08:25 -0800

    Print alphabets a to z with filtering

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: Always  (Success)
+**/
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i != q || i != e)
+			putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
