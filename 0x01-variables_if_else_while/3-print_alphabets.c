40dc5f407bdb963de0d35fc84f798e7619dc249d
Reid-T-W <rediettadesse100@gmail.com>
2022-03-11 13:02:56 -0800

    Print alphabets a to z and A to Z

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: Always  (Success)
+**/
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	for (j = 'A'; j <= 'Z'; j++)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
