2f3642f359594fcc1eff8b0d314503b94554d687
Reid-T-W <rediettadesse100@gmail.com>
2022-03-11 18:26:18 -0800

    Prinitng all the numbers of base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: Always  (Success)
+**/
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48; i <= 57; i++)
+		putchar(i);
+	for (j = 'a'; j <= 'f'; j++)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
