3b836e6977b27f00983995d3926000471a6f95a8
Reid-T-W <rediettadesse100@gmail.com>
2022-03-15 13:28:48 -0700

    Updated header file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+/**
+ *_putchar - writes the character c to stdout
+ *@c: The character to print
+ *
+ *Return: On success 1.
+ *On error, -1 is returned, and errno is set appropriately.
+ **/
+int _putchar(char c);
+/**
+ *print_alphabet - prints the alphabet in lower case
+ *
+ *Return: Always 0.
+ * */
+void print_alphabet(void);
