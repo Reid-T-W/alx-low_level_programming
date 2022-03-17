218d5f1aec963d31772af6c50f6b7cca2270e18e
Reid-T-W <rediettadesse100@gmail.com>
2022-03-16 23:53:17 -0700

    Find the second largest number given three digits

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,3 +6,12 @@
 void positive_or_negative(int n);
 
 #endif /* MAIN_H */
+/**
+ *largest_number - returns the largest 3 numbers
+ *@a: first integer
+ *@b: second integer
+ *@c: third integer
+ *Return: largest number
+ */
+
+int largest_number(int a, int b, int c);
