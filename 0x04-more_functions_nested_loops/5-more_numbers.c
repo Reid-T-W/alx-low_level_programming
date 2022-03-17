70b8ee16945b2b7409e8bd8a8eb2f871fc58c7d4
Reid-T-W <rediettadesse100@gmail.com>
2022-03-17 15:37:52 -0700

    Print 1 to 14 ten times corrected 4

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,13 +14,11 @@ void more_numbers(void)
 	for (iteration = 0; iteration < 10; iteration++)
 	{
 		for (n = 0; n <= 14; n++)
-			if (n <= 9)
-				_putchar(n + 48);
-			else
-			{
-				_putchar((n / 10) + 48);
-				_putchar((n % 10) + 48);
-			}
-	printf("\n");
+		{
+			if (n > 9)
+				_putchar((n/10) + 48);
+			_putchar((n % 10) + 48);
+		}
+	_putchar('\n');
 	}
 }
