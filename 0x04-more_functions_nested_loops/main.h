1418b0ced62c882499ca451d989abcc4907cc7b8
Reid-T-W <rediettadesse100@gmail.com>
2022-03-17 13:10:34 -0700

    Print numbers 0 through 9 except 2 and4

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -41,4 +41,11 @@ void print_numbers(void);
  *On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c);
+
+/**
+ *print_most_numbers - prints numbers 0 to 9 except 2 and 4                                              *
+ *
+ *Return: void
+ */
+void print_most_numbers(void);
 #endif /* MAIN_H */
