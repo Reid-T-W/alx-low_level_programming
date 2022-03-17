8c0b642cb5404927f57af14a35ee70dd682b74f7
Reid-T-W <rediettadesse100@gmail.com>
2022-03-17 16:33:25 -0700

    print diagonal line corrected

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,11 +9,14 @@
 void print_diagonal(int n)
 {
 	int i;
+	int j;
 
 	for (i = 0; i < n; i++)
 	{
 		if (n > 0)
 		{
+			for (j = 0; j < i; j++)
+				_putchar(' ');
 			_putchar('\\');
 			_putchar('\n');
 		}
