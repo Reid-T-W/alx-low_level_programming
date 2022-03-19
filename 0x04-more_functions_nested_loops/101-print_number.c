d0554669377a55a841f53a4e2c814de128686423
Reid-T-W <rediettadesse100@gmail.com>
2022-03-19 11:10:16 -0700

    Print an integer in reverse

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+*print_number - prints integers
+*@n: integer to be printed
+*
+*return: void
+*/
+void print_number(int n)
+{
+	int reserved = 0;
+
+	while (n / 10 != 0)
+	{
+		reserved = (reserved * 10) + (n % 10);
+		n = n / 10;
+	}
+	reserved = (reserved * 10) + (n % 10);
+	while (reserved / 10 != 0)
+	{
+		_putchar((reserved % 10) + 48);
+		reserved = reserved / 10;
+	}
+	_putchar((reserved % 10) + 48);
+	_putchar('\n');
+	return (0);
+}
