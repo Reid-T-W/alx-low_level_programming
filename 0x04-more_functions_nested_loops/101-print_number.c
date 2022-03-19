c593c5569e89cfc1e6e0ced13820b74ac8254614
Reid-T-W <rediettadesse100@gmail.com>
2022-03-19 11:42:19 -0700

    Print negative integers added

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,18 +9,24 @@
 void print_number(int n)
 {
 	int reserved = 0;
+	int original = n;
 
+	if (n < 0)
+	{
+		n = -1 * n;
+	}	
 	while (n / 10 != 0)
 	{
 		reserved = (reserved * 10) + (n % 10);
 		n = n / 10;
 	}
 	reserved = (reserved * 10) + (n % 10);
+	if (original < 0)
+		_putchar('-');
 	while (reserved / 10 != 0)
 	{
 		_putchar((reserved % 10) + 48);
 		reserved = reserved / 10;
 	}
 	_putchar((reserved % 10) + 48);
-	_putchar('\n');
 }
