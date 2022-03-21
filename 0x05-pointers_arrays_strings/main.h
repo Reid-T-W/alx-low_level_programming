f42883bcd4f47fbd3124014d7126e97b25877b89
Reid-T-W <rediettadesse100@gmail.com>
2022-03-21 11:30:09 -0700

    Change the value of the pointed variable

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+/**
+*reset_to_98 - Updates the value a pointer points to
+*@n: pointer
+*
+*Return: void
+*/
+void reset_to_98(int *n);
+#endif /* MAIN_H */
