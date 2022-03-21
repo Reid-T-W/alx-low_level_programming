674f4508bcdd405356626192fb7dc22975a16e26
Reid-T-W <rediettadesse100@gmail.com>
2022-03-21 12:01:17 -0700

    swaps the values of two integers corrected

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,10 +1,14 @@
-/**                                                           *swap_int - swaps the values of two integers
- * *@a: variable_1                                               *@b: variable_2                                               *Return: void                                                 */
+/**
+ *swap_int - swaps the values of two integers
+ *@a: variable_1
+ *@b: variable_2
+ *Return: void
+ */
 void swap_int(int *a, int *b)
 {
-	int *c;
+	int c;
 
-	c = a;
-	a = b;
-	b = c;
+	c = *a;
+	*a = *b;
+	*b = c;
 }
