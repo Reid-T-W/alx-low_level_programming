a27a2db4cb4e3bfc780c1365e7117a13535e50c7
Reid-T-W <rediettadesse100@gmail.com>
2022-03-22 11:29:51 -0700

    printing second half of a string corrected 3

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -17,7 +17,10 @@ void puts_half(char *str)
 	if ((len % 2) == 0)
 		start = len / 2;
 	else if ((len % 2) != 0)
-		start = ((len - 1) / 2) + 1;
+	{
+		start = ((len - 1) / 2);
+		start = start + 1;
+	}
 	for (i = start; i < len; i++)
 		_putchar(str[i]);
 	_putchar('\n');
