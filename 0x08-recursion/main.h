d40ec0a253d25a91c23511d53218c78df61c3572
Reid-T-W <rediettadesse100@gmail.com>
2022-03-30 13:07:19 -0700

    Function that returns the value of x raised to the power of y

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -38,4 +38,11 @@ int _strlen_recursion(char *s);
 *Return: the factorial of a number
 */
 int factorial(int n);
+/**
+*_pow_recursion - returns the value of x raised to the power of y
+*@x: parameter
+*@y: parameter
+*Return: returns the value of x rasied to y
+*/
+int _pow_recursion(int x, int y);
 #endif /* MAIN_H */
