6a0a8f81e7d7a3766cd94473014d794443cadd47
Reid-T-W <rediettadesse100@gmail.com>
2022-03-30 11:34:34 -0700

    Prints a string in reverse using recurssion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -17,4 +17,11 @@ int _putchar(char c);
 *Return: void
 */
 void _puts_recursion(char *s);
+/**
+*_print_rev_recursion - prints a string in reverse
+*@s: string to be printed in reverse
+*
+*Return: void
+*/
+void _print_rev_recursion(char *s);
 #endif /* MAIN_H */
