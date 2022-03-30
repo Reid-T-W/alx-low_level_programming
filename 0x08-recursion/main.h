69742a7658b80d4354aae8a601a51cf0aeee2d89
Reid-T-W <rediettadesse100@gmail.com>
2022-03-30 10:45:40 -0700

    Print a string with recursion

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+/**
+ *_putchar - writes the character c to stdout
+ *@c: The character to print
+ *
+ *Return: On success 1.
+ *On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+/**
+*_puts_recursion - prints a string, followed by a new line
+*@s: parameter
+*
+*Return: void
+*/
+void _puts_recursion(char *s);
+#endif /* MAIN_H */
