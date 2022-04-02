9f7294116008dbd5f1024eb25a03473ec6eb765c
Reid-T-W <rediettadesse100@gmail.com>
2022-04-02 07:46:25 -0700

    program that prints the arguments corrected 3

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -10,9 +10,9 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	for (i = 0; i <argc; i++)
+	for (i = 0; i < argc; i++)
 	{
-		printf("%s\n",argv[i]);
+		printf("%s\n"i, argv[i]);
 	}
 	return (0);
 }
