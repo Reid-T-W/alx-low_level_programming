ff837eec930a9d3bdfc4b5d611502d785663c2e6
Reid-T-W <rediettadesse100@gmail.com>
2022-04-02 07:37:17 -0700

    program that prints the arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*main - program that prints the arguments
+*@argc: param
+*@argv: param
+*Return: 0 on success
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		printf("%s\n",argv[i]);
+	}
+	return (0);
+}
