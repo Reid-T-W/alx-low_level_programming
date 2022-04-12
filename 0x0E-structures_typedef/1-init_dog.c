0cd4397e2e2c4ad7a0c21322bfd965c088530bd7
Reid-T-W <rediettadesse100@gmail.com>
2022-04-12 14:41:37 -0700

    Initialize dog struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include dog.h
+/**
+ *init_dog - function for initializing the struct dog
+ *@d:param
+ *@name: param
+ *@age: param
+ *@owner: param
+ *Return: Always 0
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d.name = name;
+	d.age = age;
+	d.owner = owner;
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,4 +14,13 @@ struct dog
 	float age;
 	char *owner;
 };
+/**
+ *init_dog - function for initializing the struct dog
+ *@d:param
+ *@name: param
+ *@age: param
+ *@owner: param
+ *Return: Always 0
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif /* DOG_HEADER */
