a59ac3981a596c36725144cfc7838f4e67fd1564
Reid-T-W <rediettadesse100@gmail.com>
2022-04-12 15:30:25 -0700

    Print values of struct corrected 3

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,16 +11,19 @@ void print_dog(struct dog *d)
 {
 	if (d == 0)
 		return;
-	if (d->name != 0)
-		printf("Name: %s\n", d->name);
-	else if (d->name == 0)
-		printf("Name: (nil)\n");
-	if (d->age != 0)
-		printf("Age: %f\n", d->age);
-	else if (d->age == 0)
-		printf("Age: (nil)\n");
-	if (d->owner != 0)
-		printf("Owner: %s\n", d->owner);
-	else if  (d->owner == 0)
-		printf("Owner: (nil)\n");
+	else
+	{
+		if (d->name != 0)
+			printf("Name: %s\n", d->name);
+		else if (d->name == 0)
+			printf("Name: (nil)\n");
+		if (d->age != 0)
+			printf("Age: %f\n", d->age);
+		else if (d->age == 0)
+			printf("Age: (nil)\n");
+		if (d->owner != 0)
+			printf("Owner: %s\n", d->owner);
+		else if  (d->owner == 0)
+			printf("Owner: (nil)\n");
+	}
 }
