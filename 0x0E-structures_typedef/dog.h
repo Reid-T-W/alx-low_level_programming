26646ddd4dcd066b5c2e65b3ac0b7c8db53b81ec
Reid-T-W <rediettadesse100@gmail.com>
2022-04-12 14:22:57 -0700

    Struct for dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_HEADER
+#define DOG_HEADER
+/**
+ * struct dog - dog struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: dog struct
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOG_HEADER */
