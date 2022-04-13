d3b9fdf0f7580e067a25f887eab123cdd610741a
Reid-T-W <rediettadesse100@gmail.com>
2022-04-13 11:50:52 -0700

    null check added

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -19,8 +19,8 @@ int (*get_op_func(char *s))(int, int)
 
 	i = 0;
 	while (i < 5)
-	{
-		if (*s == *ops[i].op)
+	{ 
+		if (*s == *ops[i].op && ops[i].op != 0 && s != 0)
 			return (ops[i].f);
 		i++;
 	}
