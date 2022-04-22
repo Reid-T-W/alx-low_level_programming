11310234f79404ae583590a36276f50fbd34c949
Reid-T-W <rediettadesse100@gmail.com>
2022-04-22 05:26:20 -0700

    functio to add node at start added

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -3,6 +3,29 @@
 #include "lists.h"
 #include <stdio.h>
 /**
+ *add_node - adds a new node
+ *@head: head of linked list
+ *@str: string to be added to node
+ *Return: address of newly added node
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+  int string_len;
+  list_t *new;
+
+  string_len = string_length(str);
+  new = malloc(sizeof(list_t));
+  if (new == 0 || str == 0)
+    {
+      return (NULL);
+    }
+  new->str = strdup(str);
+  new->len = string_len;
+  new->next = *head;
+  *head = new;
+  return (*head);
+}
+/**
  *add_node_end - adds a node to the end of a linked list
  *@head: - head of linked list
  *@str: string to be added to node
