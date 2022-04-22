28f47e63d3e97088bf5d7e982e14ac649d206309
Reid-T-W <rediettadesse100@gmail.com>
2022-04-22 05:45:37 -0700

    Error check added

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,13 +9,13 @@
  */
 int string_length(const char *str)
 {
-  int count = 0;
-  while (*str != '\0')
-    {
-      count++;
-      str = str + 1;
-    }
-  return (count);
+int count = 0;
+while (*str != '\0')
+{
+count++;
+str = str + 1;
+}
+return (count);
 }
 /**
  *add_node - adds a new node
@@ -25,20 +25,20 @@ int string_length(const char *str)
  */
 list_t *add_node(list_t **head, const char *str)
 {
-  int string_len;
-  list_t *new;
+int string_len;
+list_t *new;
 
-  string_len = string_length(str);
-  new = malloc(sizeof(list_t));
-  if (new == 0 || str == 0)
-    {
-      return (NULL);
-    }
-  new->str = strdup(str);
-  new->len = string_len;
-  new->next = *head;
-  *head = new;
-  return (*head);
+string_len = string_length(str);
+new = malloc(sizeof(list_t));
+if (new == 0 || str == 0)
+{
+return (NULL);
+}
+new->str = strdup(str);
+new->len = string_len;
+new->next = *head;
+*head = new;
+return (*head);
 }
 /**
  *add_node_end - adds a node to the end of a linked list
@@ -68,7 +68,10 @@ while ((*copy)->next != 0)
 }
 new->next = (*copy)->next;
 (*copy)->next = new;
+if (copy != 0)
 return (*copy);
+else
+return (NULL);
 }
 return (add_node(head, str));
 }
