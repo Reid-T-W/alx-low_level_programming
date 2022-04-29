0f8804294a48e4ed0b47f647056f19b152307a56
Reid-T-W <rediettadesse100@gmail.com>
2022-04-29 15:27:26 -0700

    Index out of range corrected 3

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,20 +7,15 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-unsigned long int length = 0;
-unsigned long int n_length = n;
-while (n_length > 0)
-{
-length++;
-n_length = n_length >> 1;
-}
+unsigned long int length = sizeof(unsigned long int) * 8;
+
+if (index >= length)
+return (-1);
 n = n >> index;
 if ((n & 1) == 0)
 return (0);
 else if ((n & 1) == 1)
 return (1);
-else if (index >= length)
-return (-1);
 else
 return (-1);
 }
