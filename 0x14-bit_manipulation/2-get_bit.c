a68dd57a53e6abe05816dd2dd700d4da1063b67b
Reid-T-W <rediettadesse100@gmail.com>
2022-04-29 14:33:35 -0700

    returns the value of a bit at a given index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *get_bit - function that returns the value of a bit at a given index
+ *@n: number
+ *@index: index
+ *Return: the value of the bit at index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+n = n >> index;
+if ((n & 1) == 0)
+return (0);
+else if ((n & 1) == 1)
+return (1);
+else
+return (-1);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,13 @@
 #ifndef HEADER_BIT_MANIPULATION
 #define HEADER_BIT_MANIPULATION
 /**
+ *get_bit - function that returns the value of a bit at a given index
+ *@n: number
+ *@index: index
+ *Return: the value of the bit at index
+ */
+int get_bit(unsigned long int n, unsigned int index);
+/**
  *power - calculates the power of a given number
  *@a: number
  *@b: power exponent
