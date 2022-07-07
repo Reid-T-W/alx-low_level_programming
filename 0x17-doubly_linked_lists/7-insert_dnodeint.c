132006632ec2f4fd833425bc7e988a838fefe2ec
Reid-T-W <rediettadesse100@gmail.com>
2022-07-07 14:49:26 -0700

    Task 7 Malloc fail condition corrected

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,7 +16,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new;
 
 	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
+	if (new == 0)
 	{
 		return (NULL);
 	}
