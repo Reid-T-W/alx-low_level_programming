e05e1dada55bc66d956cf1218718d18751186e7d
Reid-T-W <rediettadesse100@gmail.com>
2022-07-07 15:14:52 -0700

    Insert first and last used

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -20,6 +20,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		return (NULL);
 	}
+	if (*h == NULL && idx == 0)
+	{
+		return (add_dnodeint(&*h, n));
+	}
 	if (*h == NULL)
 	{
 		return (NULL);
@@ -28,6 +32,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		node_index = node_index->next;
 		count = count + 1;
+	}
+	if (count == idx && node_index == NULL)
+	{
+		return (add_dnodeint_end(&*h, n));
 	}
 	if (count == idx)
 	{
@@ -38,5 +46,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		node_index->prev = new;
 		return (node_index);
 	}
+
 	return (NULL);
 }
