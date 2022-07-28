c863be94bdf7bb735829f1830abe55ac7aa2d445
Reid-T-W <rediettadesse100@gmail.com>
2022-07-29 02:49:56 +0300

    Task4 Retrieve the value of a key from a hash table

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include"hash_tables.h"
+#include<stdio.h>
+#include<string.h>
+/**
+ *hash_table_get - gets the value associated with the key
+ *@key: key used to retrieve value
+ *Return: returns the key's value
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	char *value;
+	hash_node_t **get_location;
+	hash_node_t *list_iterator = NULL;
+
+	index = key_index((const unsigned char *)key, ht->size);
+	get_location = ht->array + index;
+	list_iterator = *get_location;
+	while (list_iterator != NULL)
+	{
+		if (strcmp((list_iterator)->key, key) == 0)
+		{
+			value = (list_iterator)->value;
+			return value;
+		}
+		list_iterator = list_iterator->next;
+	}
+	return NULL;
+}
