2165bce807c5db04ed58e48f526ecf8092efcd7f
Reid-T-W <rediettadesse100@gmail.com>
2022-07-30 00:22:35 +0300

    Values are duplicated

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,6 +1,7 @@
 #include "hash_tables.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 /**
  *hash_table_set - adds an element to the hash table
  *@ht: hash table
@@ -13,6 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *node;
 	hash_node_t **insert_location;
+	char *dup_value;
 
 	if (ht == NULL)
 	{
@@ -28,7 +30,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 	node->key = (char *)key;
-	node->value = (char *)value;
+	dup_value = strdup(value);
+	node->value = dup_value;
 	index = key_index((const unsigned char *)key, ht->size);
 	insert_location = ht->array + index;
 	node->next = *insert_location;
