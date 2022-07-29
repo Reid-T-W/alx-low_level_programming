a96cd7f069df2f773f46e68d79d1454dbe1bb029
Reid-T-W <rediettadesse100@gmail.com>
2022-07-30 00:56:04 +0300

    Correction on allocated size of array

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,19 +7,19 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table;
+	hash_table_t *table = NULL;
 
 	if (size == 0)
 	{
 		return (NULL);
 	}
-	table = malloc(size * sizeof(table));
+	table = malloc(sizeof(table));
 	if (table == NULL)
 	{
 		return (NULL);
 	}
 	table->size = size;
-	table->array = malloc(size * sizeof(*(table->array)));
+	table->array = malloc(size * sizeof(*table->array));
 	if ((table->array) == NULL)
 	{
 		return (NULL);
