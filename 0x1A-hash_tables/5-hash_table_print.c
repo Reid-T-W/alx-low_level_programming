cd3fafabb5be68bcc1023572ca3838ee61bb808d
Reid-T-W <rediettadesse100@gmail.com>
2022-07-29 14:43:57 +0300

    Print all elements of a hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,38 @@
+#include"hash_tables.h"
+#include<stdio.h>
+/**
+ *hash_table_print - prints a hash table
+ *@ht: hash table
+ *
+ *Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t **get_location = NULL;
+	hash_node_t *list_iterator = NULL;
+	unsigned long int index = 0;
+	int first_print = 1;
+
+	if (ht == NULL)
+	{
+		return;
+	}
+	printf("{");
+	while (index < ht->size)
+	{
+		get_location = ht->array + index;
+		if (*get_location != NULL)
+		{
+			if (!first_print)
+			{
+				printf(", ");
+			}
+			list_iterator = *get_location;
+			printf("'%s': '%s'", list_iterator->key, list_iterator->value);
+			first_print = 0;
+		}
+		index = index + 1;
+	}
+	printf("}\n");
+
+}
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,5 +1,5 @@
 #ifndef HEADER_HASH_TABLE
-#define HASH_TABLE
+#define HEADER_HASH_TABLE
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -10,9 +10,9 @@
  */
 typedef struct hash_node_s
 {
-  char *key;
-  char *value;
-  struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -25,8 +25,8 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-  unsigned long int size;
-  hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 /**
  *hash_table_create - creates a hash table
@@ -38,4 +38,6 @@ unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
-#endif /*HASH_TABLE*/
+void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
+#endif /*HEADER_HASH_TABLE*/
