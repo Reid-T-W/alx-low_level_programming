9363ddcad90e926690957c3f1e228923f63f8cbe
Reid-T-W <rediettadesse100@gmail.com>
2022-11-01 21:05:57 +0300

    Correction to jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -33,7 +33,7 @@ int jump_search(int *array, size_t size, int value)
 	{
 		printf("Value checked array[%ld] = [%d]\n", prev, array[prev]);
 		prev++;
-		if (size <= prev)
+		if (size <= prev || prev > point)
 			return (-1);
 	}
 	printf("Value checked array[%ld] = [%d]\n", prev, array[prev]);
