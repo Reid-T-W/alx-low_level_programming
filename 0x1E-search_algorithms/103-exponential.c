03069a2d0e49a611a7a4ee8904a157c5dace1ca2
Reid-T-W <rediettadesse100@gmail.com>
2022-11-02 23:40:41 +0300

    Null changed to 0

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -14,7 +14,7 @@ int exponential_search(int *array, size_t size, int value)
 	size_t exp;
 	size_t min;
 
-	if (array == NULL)
+	if (array == 0)
 		return (-1);
 	exp = 1;
 	while (array[exp] < value && exp < size)
